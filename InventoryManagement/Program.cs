92908854722d0590ae0fe4bce0b0b0c8f436c584
ahamedsahib <adsahib39@gmail.com>
2021-07-06 19:18:34 +0530

    [Ahamed]Manging Inventory by Adding,Deleting,Displaying Products

--- InventoryManagement/Program.cs
+++ InventoryManagement/Program.cs
@@ -16,23 +16,102 @@ namespace InventoryManagement
             InventoryUtility utility = JsonConvert.DeserializeObject<InventoryUtility>(File.ReadAllText(jsonFilepath));
 
             Console.WriteLine("---------MENU-------");
-            Console.WriteLine("1.Display Rice \n2.Display Pulse\n3.Display Wheat\n4.Cancel"); 
-            Console.WriteLine("Choose Your Option"); 
+            Console.WriteLine("1.Display Inventories \n2.Add Inventories\n3.Edit Inventories\n4.Delete Inventories\n5.Exit\nEnter Your Option");
             switch (Convert.ToInt32(Console.ReadLine()))
             {
                 case 1:
-                    manager.DisplayProducts(utility.riceList);
+                    Console.WriteLine("1.Display Rice \n2.Display Pulse\n3.Display Wheat\n4.Exit\nEnter Your Option");
+                    switch (Convert.ToInt32(Console.ReadLine()))
+                    {
+                        case 1:
+                            manager.DisplayProducts(utility.riceList);
+
+
+                            break;
+                        case 2:
+                            manager.DisplayProducts(utility.pulseList);
+
+                            break;
+                        case 3:
+                            manager.DisplayProducts(utility.wheatList);
+
+                            break;
+                        case 4:
+                            Console.WriteLine("Exited");
+                            break;
+                    }
                     break;
                 case 2:
-                    manager.DisplayProducts(utility.pulseList);
+                    Console.WriteLine("1.Add Rice \n2.Add Pulse\n3.Add Wheat\n4.Exit\nEnter Your Option");
+                    switch (Convert.ToInt32(Console.ReadLine()))
+                    {
+                        case 1:
+                            manager.AddInventory(utility.riceList);
+                            File.WriteAllText(jsonFilepath, JsonConvert.SerializeObject(utility));
+
+                            break;
+                        case 2:
+                            manager.AddInventory(utility.pulseList);
+                            File.WriteAllText(jsonFilepath, JsonConvert.SerializeObject(utility));
+                            break;
+                        case 3:
+                            manager.AddInventory(utility.wheatList);
+                            File.WriteAllText(jsonFilepath, JsonConvert.SerializeObject(utility));
+                            break;
+                        case 4:
+                            Console.WriteLine("Exited");
+                            break;
+                    }
                     break;
                 case 3:
-                    manager.DisplayProducts(utility.wheatList);
+                    Console.WriteLine("1.Edit Rice \n2.Edit Pulse\n3.Edit Wheat\n4.Exit\nEnter Your Option");
+                    switch (Convert.ToInt32(Console.ReadLine()))
+                    {
+                        case 1:
+                            manager.EditInventory(utility.riceList);
+                            File.WriteAllText(jsonFilepath, JsonConvert.SerializeObject(utility));
+                            break;
+                        case 2:
+                            manager.EditInventory(utility.pulseList);
+                            File.WriteAllText(jsonFilepath, JsonConvert.SerializeObject(utility));
+                            break;
+                        case 3:
+                            manager.EditInventory(utility.wheatList);
+                            File.WriteAllText(jsonFilepath, JsonConvert.SerializeObject(utility));
+                            break;
+                        case 4:
+                            Console.WriteLine("Exited");
+                            break;
+                    }
                     break;
                 case 4:
-                    Console.WriteLine("Cancel");
+                    Console.WriteLine("1.Delete Rice \n2.Delete Pulse\n3.Delete Wheat\n4.Exit\nEnter Your Option");
+                    switch (Convert.ToInt32(Console.ReadLine()))
+                    {
+                        case 1:
+                            manager.DeleteInventory(utility.riceList);
+                            File.WriteAllText(jsonFilepath, JsonConvert.SerializeObject(utility));
+                            break;
+                        case 2:
+                            manager.DeleteInventory(utility.pulseList);
+                            File.WriteAllText(jsonFilepath, JsonConvert.SerializeObject(utility));
+                            break;
+                        case 3:
+                            manager.DeleteInventory(utility.wheatList);
+                            File.WriteAllText(jsonFilepath, JsonConvert.SerializeObject(utility));
+                            break;
+                        case 4:
+                            Console.WriteLine("Exited");
+                            break;
+                    }
+
+                    break;
+                case 5:
+                    Console.WriteLine("Exited");
                     break;
             }
+
+
         }
     }
 }
